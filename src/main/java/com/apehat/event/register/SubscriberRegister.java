3a7dea8ce96a5f72bf7136cf8a2854b8a066083f
code-mac <hpf@apehat.com>
2018-04-22 01:03:29 +0800

    Reformat docs.

--- src/main/java/com/apehat/event/register/SubscriberRegister.java
+++ src/main/java/com/apehat/event/register/SubscriberRegister.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.apehat.event.internal.subscriber;
+package com.apehat.event.register;
 
 import com.apehat.event.Event;
 import com.apehat.event.SubscribeScope;
@@ -31,11 +31,13 @@ public interface SubscriberRegister {
     /**
      * Register a subscriber to current register.
      * <p>
-     * If the scope of subscriber is {@link SubscribeScope#GLOBAL}
-     * the register will be find by all register.
+     * If the scope of subscriber is {@link SubscribeScope#GLOBAL} the register
+     * will be find by all register.
      *
-     * @param subscriber the subscriber to register
-     * @throws NullPointerException the specified subscriber is null
+     * @param subscriber
+     *         the subscriber to register
+     * @throws NullPointerException
+     *         the specified subscriber is null
      * @see SubscribeScope
      */
     <T extends Event> void register(Subscriber<T> subscriber);
@@ -43,36 +45,44 @@ public interface SubscriberRegister {
     /**
      * Unregister the specified subscriber form this.
      *
-     * @param subscriber the subscriber to unregister
-     * @throws NullPointerException the specified subscriber is null
+     * @param subscriber
+     *         the subscriber to unregister
+     * @throws NullPointerException
+     *         the specified subscriber is null
      */
     <T extends Event> void unregister(Subscriber<T> subscriber);
 
     /**
      * Returns the subscribers of specified event.
      *
-     * @param event the event to get the subscribers
-     * @param <T>   the type of event
+     * @param event
+     *         the event to get the subscribers
+     * @param <T>
+     *         the type of event
      * @return the subscribers of specified event, or empty set, if does not
      * have subscribers of specified event
-     * @throws NullPointerException the specified event is null
+     * @throws NullPointerException
+     *         the specified event is null
      */
     <T extends Event> Set<Subscriber<? super T>> subscribersOf(T event);
 
     /**
      * Determine whether the current register contains specified subscriber.
      *
-     * @param subscriber the subscriber to check
+     * @param subscriber
+     *         the subscriber to check
      * @return true, if the current register contains specified subscriber,
      * else, otherwise false
-     * @throws NullPointerException specified subscriber is null
+     * @throws NullPointerException
+     *         specified subscriber is null
      */
     boolean contains(Subscriber<?> subscriber);
 
     /**
      * Determine whether specified subscriber can register by this register.
      *
-     * @param subscriber the subscriber to check
+     * @param subscriber
+     *         the subscriber to check
      * @return true, current register can register specified subscriber;
      * otherwise false
      */
