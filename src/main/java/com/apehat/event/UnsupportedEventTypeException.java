ef404051cb4d53c56701e781d5efa8554eee19e3
code-mac <hpf@apehat.com>
2018-04-23 22:17:24 +0800

    Create branch 1.0.1-snapshot

---
+++ src/main/java/com/apehat/event/UnsupportedEventTypeException.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Apehat.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apehat.event;
+
+/**
+ * @author hanpengfei
+ * @since 1.0
+ */
+public class UnsupportedEventTypeException extends IllegalArgumentException {
+
+    private final Class<?> supportedEventType;
+    private final Class<?> specifiedEventType;
+
+    public UnsupportedEventTypeException(Class<?> supportedEventType,
+                                         Class<?> specifiedEventType) {
+        assert !supportedEventType.isAssignableFrom(specifiedEventType);
+        this.supportedEventType = supportedEventType;
+        this.specifiedEventType = specifiedEventType;
+    }
+
+    public Class<?> getSupportedEventType() {
+        return supportedEventType;
+    }
+
+    public Class<?> getSpecifiedEventType() {
+        return specifiedEventType;
+    }
+}
